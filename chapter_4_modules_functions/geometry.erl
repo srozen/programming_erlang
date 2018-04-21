3619faad346f4674181407e873f3274e9f616aea
Samuel Monroe <spat.monroe@gmail.com>
2018-04-21 16:16:40 +0200

    Restarting

--- chapter_4_modules_functions/geometry.erl
+++
@@ -1,7 +0,0 @@
--module (geometry).
--export ([area/1]).
-
-area({rectangle, Width, Height}) ->
-  Width * Height;
-area({square, Side}) ->
-  Side * Side.
--- chapter_4_modules_functions/mylists.erl
+++
@@ -1,7 +0,0 @@
--module (mylists).
--export ([sum/1, map/2]).
-
-sum([H|T]) -> H + sum(T);
-sum([]) -> 0.
-
-map(F, L) -> [F(X) || X <- L].
