863d9212fa97b9a28876ffd85bdb4365fb59c279
Samuel Monroe <spat.monroe@gmail.com>
2018-04-27 10:35:29 +0200

    Chapter 4 final exercices finished

--- chapter_4_modules_functions/math_functions.erl
+++ chapter_4_modules_functions/math_functions.erl
@@ -1,5 +1,5 @@
 -module (math_functions).
--export ([even/1, odd/1, filter/2]).
+-export ([even/1, odd/1, filter/2, split/1, split2/1]).
 
 even(X) ->
   if
@@ -22,3 +22,20 @@ filter(F, [H|T]) ->
   end;
 
 filter(_, []) -> [].
+
+split(L) ->
+  Odds = [X || X <- L, (X rem 2) =:= 1],
+  Even = [X || X <- L, (X rem 2) =:= 0],
+  {Odds, Even}.
+
+split2(L) ->
+  split_acc(L, [], []).
+
+split_acc([H|T], Odds, Even) ->
+  case (H rem 2) of
+    0 -> split_acc(T, Odds, [H|Even]);
+    1 -> split_acc(T, [H|Odds], Even)
+  end;
+
+split_acc([], Odds, Even) ->
+  {lists:reverse(Odds), lists:reverse(Even)}.
